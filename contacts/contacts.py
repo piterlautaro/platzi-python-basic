b3091f99c32277b487d7dab1f4f75f8ca77b1549
Pedro <pedro@socialab.com>
2020-07-25 01:21:06 -0400

    Update contacts.py

--- contacts/contacts.py
+++ contacts/contacts.py
@@ -1,9 +1,11 @@
+import csv
+
 class Contact:
 
 	def __init__(self, name, phone, email):
-		self._name = name
-		self._phone = phone
-		self._email = email
+		self.name = name
+		self.phone = phone
+		self.email = email
 
 class ContactBook:
 
@@ -11,7 +13,57 @@ class ContactBook:
 		self._contacts = []
 
 	def add(self, name, phone, email):
-		print('name: {}, phone: {}, email: {}'.format(name, phone, email))
+		contact = Contact(name, phone, email)
+		self._contacts.append(contact)
+
+	def show_all(self):
+		for contact in self._contacts:
+			self._print_contact(contact)
+
+	def _print_contact(self, contact):
+		print('---*---*---*---*---*---*---*---*---*---')
+		print('Nombre: {}'.format(contact.name))
+		print('Teléfono: {}'.format(contact.phone))
+		print('Email: {}'.format(contact.email))
+		print('---*---*---*---*---*---*---*---*---*---')
+
+	def delete(self, name):
+		for idx, contact in enumerate(self._contacts):
+			if contact.name.lower() == name.lower():
+				del self._contacts[idx]
+				print('Contacto eliminado')
+				break
+
+	def search(self, name):
+		for contact in self._contacts:
+			if contact.name.lower() == name.lower():
+				self._print_contact(contact)
+				break
+		else:
+			self._not_found()
+
+	def _save(self):
+		with open('contacts.csv','w') as f:
+			writer = csv.writer(f)
+			writer.writerow(('name','phone','email'))
+
+			for contact in self._contacts:
+				writer.writerow((contact.name, contact.phone, contact.email))
+
+	def _not_found(self):
+		print('*****')
+		print('No encontrado :(')
+		print('*****')
+
+	def update(self, name, phone, email):
+		for contact in self._contacts:
+			if contact.name.lower() == name.lower():
+				contact.phone = phone
+				contact.email = email
+				break
+		else:
+			self._not_found()
+
 
 def run():
 
@@ -37,16 +89,22 @@ def run():
 			contact_book.add(name, phone, email)
 
 		elif command == 'ac':
-			print('actuaizar contacto')
+			name = str(input('Escribe el nombre del contacto: '))
+			phone = str(input('Escribe el teléfono del contacto: '))
+			email = str(input('Escribe el email del contacto: '))
+
+			contact_book.update(name, phone, email)
 
 		elif command == 'b':
-			print('buscar contacto')
+			name = str(input('Nombre: '))
+			contact_book.search(name)
 
 		elif command == 'e':
-			print('eliminar contacto')
+			name = str(input('Nombre: '))
+			contact_book.delete(name)
 
 		elif command == 'l':
-			print('listar contactos')
+			contact_book.show_all()
 
 		else:
 			print('salir')
